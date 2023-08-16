ca7c47459ed1b0ea1c7426c772fc5003a4aaf022
rut patel <rutpatel464@gmail.com>
2023-08-16 22:16:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pr_2_080_5b6/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.pr_2_080_5B6
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import android.util.Log
+import com.example.pr_2_080_5b6.R
+import com.google.android.material.snackbar.Snackbar
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    override fun onStart()
+    {
+        super.onStart()
+        displayMsg("Onstart Method called")
+    }
+    override fun onResume() {
+        super.onResume()
+        displayMsg("OnResume Method called")
+    }
+    override fun onPause() {
+        super.onPause()
+        displayMsg("onPause Method called")
+    }
+    override fun onStop() {
+        super.onStop()
+        displayMsg("onStop Method called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        displayMsg("onDestroy Method called")
+    }
+    fun displayMsg(msg:String){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.rj), msg,Snackbar.LENGTH_SHORT).show()
+        Log.i("MainActivity", msg)
+    }
+}
